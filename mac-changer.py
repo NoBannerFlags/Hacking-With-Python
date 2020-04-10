0a5fd7c97109402528840d502f4ddece8efd34c2
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 17:07:51 -0600

    allows mac-changer.py to read output
    mac-changer can now read the output and displays it.
    also added error messages on common errors.

--- mac-changer.py
+++ mac-changer.py
@@ -3,27 +3,30 @@
 import subprocess
 import optparse
 
-def change_mac(interface, desired_mac):
-    print("[+] Changing MAC address for " + interface + " to " + desired_Mac)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change mac address on")
+    parser.add_option("-m", "--mac", dest="desired_mac", help="Desired new mac address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    if not options.desired_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options
 
+def change_mac(interface, desired_Mac):
+    print("[+] Changing MAC address for " + interface + " to " + desired_Mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", desired_Mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest= "interface", help="Interface to change mac address on")
-parser.add_option("-m", "--mac", dest= "desired_mac", help="Desired new mac address")
-
-(options, arguments) = parser.parse_args()
-
+(options)= get_arguments()
 print("Note: this program only works on linux!")
-
 # interface = input("Interface > ")
 # interface = options.interface
-
 # desired_Mac = input("What is your desired mac? ex:\n00:00:00:00:00:00\n")
 # desired_Mac = options.desired_mac
+# change_mac(options.interface, options.desired_mac)
 
-change_mac(options.interface, options.desired_mac)
-
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result)
