f0851633719916f0c89da98de31487a1e4cc873e
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:32:29 -0600

    Made mac-changer.py more secure

--- mac-changer.py
+++ mac-changer.py
@@ -3,14 +3,19 @@
 import subprocess
 
 print("Note: this program only works on linux!")
+
 # interface = "wlan0"
 interface = input("Interface > ")
+
 # desired_Mac = "00:11:22:33:44:55"
 desired_Mac = input("What is your desired mac? ex:\n00:00:00:00:00:00\n")
-print("[+] Changing MAC address for "+interface+" to "+desired_Mac)
-# print("Starting Mac Changer")
-subprocess.call("ifconfig "+interface+" down", shell=True)
-subprocess.call("ifconfig "+interface+" hw ether "+desired_Mac, shell=True)
-subprocess.call("ifconfig "+interface+" up", shell=True)
-print("Mac changer finished")
 
+print("[+] Changing MAC address for "+interface+" to "+desired_Mac)
+
+# subprocess.call("ifconfig "+interface+" down", shell=True)
+# subprocess.call("ifconfig "+interface+" hw ether "+desired_Mac, shell=True)
+# subprocess.call("ifconfig "+interface+" up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", desired_Mac])
+subprocess.call(["ifconfig", interface, "up"])
