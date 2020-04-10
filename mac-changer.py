aecbf0d157c814f3c2d3b6b76e319c88119f5bd8
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:15:46 -0600

    added variables to mac-changer.py
    - added variables interface and desired_mac, interface replaces the wlan0 previously in the code, desired_mac replaces the mac adress.
    
    - added print statements to detail what is happening (todo: comment out the print statements once code is tested)
    
    - added a new line to Good Commands.txt

--- mac-changer.py
+++ mac-changer.py
@@ -3,9 +3,14 @@
 import subprocess
 
 print("Note: this program only works on linux!")
+print("Defining variables")
+interface = "wlan0"
+print("interface: "+interface)
+desired_Mac = "00:11:22:33:44:55"
+print("desired_mac: "+desired_Mac)
 print("Starting Mac Changer")
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig "+interface+" down", shell=True)
+subprocess.call("ifconfig "+interface+" hw ether "+desired_Mac, shell=True)
+subprocess.call("ifconfig "+interface+" up", shell=True)
 print("Mac changer finished")
 
