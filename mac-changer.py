e508be58450043bdc1ddc8bbd467fdc94d1076ac
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:26:30 -0600

    added user input
    - added user input for interface & desired_mac in mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess
 
 print("Note: this program only works on linux!")
-# print("Defining variables")
-interface = "wlan0"
-desired_Mac = "00:11:22:33:44:55"
+# interface = "wlan0"
+interface = input("Interface > ")
+# desired_Mac = "00:11:22:33:44:55"
+desired_Mac = input("What is your desired mac? ex:\n00:00:00:00:00:00\n")
 print("[+] Changing MAC address for "+interface+" to "+desired_Mac)
 # print("Starting Mac Changer")
 subprocess.call("ifconfig "+interface+" down", shell=True)
