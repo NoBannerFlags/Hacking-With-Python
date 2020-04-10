0a83e76155bd1176cd15bb20d74b2150391699bd
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:18:48 -0600

    removed undesired print statements
    simplified the print statements in mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -3,12 +3,11 @@
 import subprocess
 
 print("Note: this program only works on linux!")
-print("Defining variables")
+# print("Defining variables")
 interface = "wlan0"
-print("interface: "+interface)
 desired_Mac = "00:11:22:33:44:55"
-print("desired_mac: "+desired_Mac)
-print("Starting Mac Changer")
+print("[+] Changing MAC address for "+interface+" to "+desired_Mac)
+# print("Starting Mac Changer")
 subprocess.call("ifconfig "+interface+" down", shell=True)
 subprocess.call("ifconfig "+interface+" hw ether "+desired_Mac, shell=True)
 subprocess.call("ifconfig "+interface+" up", shell=True)
