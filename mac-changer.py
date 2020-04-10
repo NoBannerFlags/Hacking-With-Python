13521d756a9778b95e33ab57ee89893d78e9bf29
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:08:06 -0600

    Update mac-changer.py
    fixed a bug where the interface was not defined in the line that changes the address

--- mac-changer.py
+++ mac-changer.py
@@ -5,7 +5,7 @@ import subprocess
 print("Note: this program only works on linux!")
 print("Starting Mac Changer")
 subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
 subprocess.call("ifconfig wlan0 up", shell=True)
 print("Mac changer finished")
 
