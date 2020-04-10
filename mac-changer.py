cf367ca6a4ed64d8a0fb1dd64b51cb23b821abd7
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-10 16:44:11 -0600

    Update mac-changer.py
    allows command line arguments to initialize variables

--- mac-changer.py
+++ mac-changer.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest= "interface", help="Interface to change mac address on")
+parser.add_option("-m", "--mac", dest= "desired_mac", help="Desired new mac address")
+
+(options, arguments) = parser.parse_args()
 
 print("Note: this program only works on linux!")
 
-# interface = "wlan0"
-interface = input("Interface > ")
+# interface = input("Interface > ")
+interface = options.interface
 
-# desired_Mac = "00:11:22:33:44:55"
-desired_Mac = input("What is your desired mac? ex:\n00:00:00:00:00:00\n")
+# desired_Mac = input("What is your desired mac? ex:\n00:00:00:00:00:00\n")
+desired_Mac = options.desired_mac
 
 print("[+] Changing MAC address for "+interface+" to "+desired_Mac)
-
-# subprocess.call("ifconfig "+interface+" down", shell=True)
-# subprocess.call("ifconfig "+interface+" hw ether "+desired_Mac, shell=True)
-# subprocess.call("ifconfig "+interface+" up", shell=True)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", desired_Mac])
