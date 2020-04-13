6748b679c0a876ec56da7af60a4f80a0ff7f3e45
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-13 14:40:06 -0600

    updated mac-changer.py
    mac-changer.py now retrieves the value of the printed ifconfig and finds the mac address inside. if no mac address is found, it notifies the user, otherwise, it prints the mac address.

--- mac-changer.py
+++ mac-changer.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import optparse
+import re
+
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -14,13 +16,17 @@ def get_arguments():
         parser.error("[-] Please specify a new mac, use --help for more info.")
     return options
 
+
 def change_mac(interface, desired_Mac):
     print("[+] Changing MAC address for " + interface + " to " + desired_Mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", desired_Mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options)= get_arguments()
+
+options= get_arguments()
+
+
 print("Note: this program only works on linux!")
 # interface = input("Interface > ")
 # interface = options.interface
@@ -30,3 +36,10 @@ print("Note: this program only works on linux!")
 
 ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
 print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address_search_result:
+    print(mac_address_search_result.group(0))
+else:
+    print("[-]Could not find mac address!")
