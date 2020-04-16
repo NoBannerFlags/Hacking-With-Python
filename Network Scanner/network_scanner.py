22d174a3b6f83ca63e7c378329ae5345420742f1
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-16 16:57:52 -0600

    general housekeeping for network scanner

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -4,10 +4,9 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    arp_request = scapy.ARP()
-    arp_request.pdst = ip
+    arp_request = scapy.ARP(pdst=ip)
     print(arp_request.summary())
-    #scapy.ls(scapy.ARP())
+
 
 
 scan("192.168.1.1/24")
