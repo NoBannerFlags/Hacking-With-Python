2ec3159bc32ea58c8e80bebab785e75cfa07dbbc
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-16 16:56:31 -0600

    updated network_scanner.py

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -5,8 +5,9 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP()
+    arp_request.pdst = ip
     print(arp_request.summary())
-    scapy.ls(scapy.ARP())
+    #scapy.ls(scapy.ARP())
 
 
 scan("192.168.1.1/24")
