57b176bc5b7ab6a4d95b55386da757370959b6ef
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-23 12:53:07 -0600

    Update network_scanner.py
    get responses to packets in 2 lists,
    
    answered_list contains all of the answers
    
    unanswered_list contains all of the values that did not recieve an answer

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -5,11 +5,16 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
+
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
+
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+    # arp_request_broadcast.show()
+    # use for debug
+
+    answered_list, unanswerd_list = scapy.srp(arp_request_broadcast, timeout =1)
+    print(answered_list.summary())
+
 
 scan("127.0.0.1/24")
 # this is a placeholder, not an actual IP - NOTE: changed to loopback
