2f0b4f8e9894cac8d0f289b775d4aaae2261eea4
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-04-23 12:44:51 -0600

    updates to network scanner

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -5,8 +5,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 
-
-
-scan("192.168.1.1/24")
+scan("127.0.0.1/24")
+# this is a placeholder, not an actual IP - NOTE: changed to loopback
+# TODO - remove placeholder
